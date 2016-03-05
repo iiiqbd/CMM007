491e46bd27ff17cf9b29a7e75b6be7b539a06817
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-03-05 18:57:37 +0000

    ...

--- w06/02phpifelse/index.php
+++ w06/02phpifelse/index.php
@@ -1,18 +1,14 @@
-///*
-// Created by PhpStorm.
-// User: Uche
-// Date: 005, 05 Mar 2016
-// Time: 17:23
-// */
+/*
+ Created by PhpStorm.
+ User: Uche
+ Date: 005, 05 Mar 2016
+ Time: 17:23
+ */
 
 <!DOCTYPE html>
 <html>
 <head>
   <title>
-PHP Practice
-</title>
-</head>
-
 <body>
   <p>
     <?php
