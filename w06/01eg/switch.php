f6eb51de6767a16c4e92f0ca2b92a2b03847a0e1
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-03-05 19:36:31 +0000

    ...

---
+++ w06/01eg/switch.php
@@ -0,0 +1,39 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>
+    PHP Practice
+  </title>
+</head>
+
+<body>
+  <p>
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: Uche
+     * Date: 005, 05 Mar 2016
+     * Time: 17:57
+     */
+      $myage = 15;
+      $myname = "Sam";
+
+      echo "Your name is " . $myname . " you can buy";
+      if ($myage > 16) {
+          print " Specs!";
+      }
+      else if ($myage > 18) {
+          print "Mugs!";
+      }
+      else if ($myage > 21) {
+          print " Sausage rolls!";
+      }
+      else {
+          print " nothing, because you are under-aged!";
+      }
+    ?>
+  </p>
+</body>
+</html>
