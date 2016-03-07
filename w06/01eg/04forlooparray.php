a0626c164fdc241b34ac2b7ad2f17722e6d6678c
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-03-07 11:08:24 +0000

    0

--- w06/01eg/04forlooparray.php
+++ w06/01eg/04forlooparray.php
@@ -7,29 +7,30 @@
 </head>
 
 <body>
-    <p>
-        <?php
-
-            for ($day = 1; $day <= 30; $day++) {
-
-                if ($day = 1) {
-                    print "no products are available";
-                }
-                if (($day % 2 == 0) && ($day != 4)) {
-                    print "specs are available";;
-                } else if ($day % 3 == 0) {
-                    print "mugs are available";
-                } else if (($day % 2 == 0) && ($day == 4)) {
-                    print "specs and sausage rolls are available";
-                } else if ($day % 5 == 0) {
-                    $day = 5;
-                }
-            }
-        ?>
-    </p>
+<!--    <p>-->
+<!--        --><?php
+//
+//            for ($day = 1; $day <= 30; $day++) {
+//
+//                if ($day = 1) {
+//                    print "no products are available";
+//                }
+//                if (($day % 2 == 0) && ($day != 4)) {
+//                    print "specs are available";;
+//                } else if ($day % 3 == 0) {
+//                    print "mugs are available";
+//                } else if (($day % 2 == 0) && ($day == 4)) {
+//                    print "specs and sausage rolls are available";
+//                } else if ($day % 5 == 0) {
+//                    $day = 5;
+//                }
+//            }
+//        ?>
+<!--    </p>-->
 
     Welcome <?php echo $_POST["name"]; ?><br>
     Your email address is: <?php echo $_POST["email"]; ?>
+    Your comment is: <?php echo $_POST["comments"]; ?>
 </body>
 
 </html>
