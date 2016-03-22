e2519f63e39635985f315b8881299bef3bf9a308
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-03-22 10:35:15 +0000

    fdhg

--- w07/home.php
+++ w07/home.php
@@ -8,4 +8,22 @@
 <body>
     <h1>Hello!</h1>
 </body>
+
+<?php  // displays the currently logged in user ?>
+
+<?php  $sql = "SELECT * FROM users"; // displays all the users of the site
+        $result = mysqli_query($db, $sql);
+        while($row = fetch_array_assoc($result)) {
+            $username = $row['username'];
+            echo "<p>" . $username . "</p>";
+        }
+
+/*
+$result = mysqli_query($db, $sql);
+while($row = mysql_fetch_array($result, MYSQLI_ASSOC)) {
+
+}
+*/
+?>
+
 </html>
