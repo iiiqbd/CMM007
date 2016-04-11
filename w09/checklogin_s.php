5d3b12b2dc45afc6e6f3b250eec5fd1455445fc6
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-04-11 09:54:24 +0100

    cks+sess

---
+++ w09/checklogin_s.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0303077
+ * Date: 11/04/2016
+ * Time: 09:11
+ */
+
+session_start();
+$accesslevel = $_SESSION['access_level_session'];
+
+displayAccessLevelInfrmation($accesslevel);
+
+function displayAccessLevelInfrmation($accesslevel)
+{
+    if($accesslevel == "standarduser") {
+        echo "<p style = \"background-color: lightgreen\">You are currently logged in as a standard user</p>";
+    }
+    elseif ($accesslevel == "root") {
+        echo "<p style = \"background-color: red\">You are currently logged in as a root user</p>";
+        echo "<p style = \"background-color: red\">You now have access to additional admin. features.</p>";
+    }
+}
+
+?>
---
+++ w09/loggedin_s.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0303077
+ * Date: 11/04/2016
+ * Time: 09:10
+ */
+
+$username = $_POST['name'];
+$password = $_POST['password'];
+
+if ($username == "user" && $password == "pass");
+{
+    session_start();
+    $_SESSION['access_level_session'] = 'standarduser';
+}
+
+header('location: loggedin_s.php');
+
+?>
