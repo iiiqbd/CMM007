78bd52752653891831b799b48ff92af7972d80f6
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-04-25 10:38:52 +0100

    mal1

--- w10/showbugs.php
+++ w10/showbugs.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Bug Tracker</title>
+    <title>Show Bugs</title>
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/unsemantic-grid-responsive-tablet.css">
 
@@ -13,47 +13,44 @@
 <header>
     <img src="assets/images/logo.png" alt="Bug tracker logo" id="logo">
     <h1>Bug Tracker</h1>
-    <h1>Keeping track of all the pesky little bugs</h1>
+    <h1>Here are all the bugs we have information on</h1>
 </header>
 <!-- end of header-->
 
 <!-- start of main-->
 <main>
 
-    <!-- start of aside-->
-    <aside>
-        <!-- start of navigation-->
-        <nav>
-            <ul>
-                <li><a href="bugs.html">All Bug Items</a></li>
-                <li><a href="bugs.html">Android Bugs</a></li>
-                <li><a href="bugs.html">iOS Bugs</a></li>
-                <li><a href="bugs.html">Windows Bugs</a></li>
-                <li><a href="bugs.html">Insert Bug</a></li>
-            </ul>
-        </nav>
-        <!-- end of navigation-->
-    </aside>
-    <!-- end of aside-->
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: 0303077
+     * Date: 25/04/2016
+     * Time: 10:11
+     */
+    include('db_conn.php');
+    $sql = "SELECT bugName, bugCategory, bugSummary
+    FROM bugs";
+    $result = mysqli_query($conn,$sql);
+    echo "
+    <table>
+        <tr>
+            <th>Bug Name</th>
+            <th>Bug Category</th>
+            <th>Bug Summary</th>
+        </tr> ";
 
-    <!-- start of section-->
-    <h2>Bug Name</h2>
-    <h3>Bug Category</h3>
-    <p>
-        Bug Summary Bug Summary Bug Summary Bug Summary
-        Bug Summary Bug Summary Bug Summary Bug Summary
-        Bug Summary Bug Summary Bug Summary Bug Summary
-    </p>
-    hr
-    <h2>Bug Name</h2>
-    <h3>Bug Category</h3>
-    <p>
-        Bug Summary Bug Summary Bug Summary Bug Summary
-        Bug Summary Bug Summary Bug Summary Bug Summary
-        Bug Summary Bug Summary Bug Summary Bug Summary
-    </p>
+        if(mysqli_num_rows($result)>0){
+        while($row = mysqli_fetch_assoc($result)){
+        echo "<tr>
+            <td>{$row['bugName']}</td>
+            <td>{$row['bugCategory']}</td>
+            <td>{$row['bugSummary']}</td>
+        </tr>";
+        }
+        }
+        echo "</table>";
+    ?>
 
-    <!-- end of section-->
 
     <!-- end of main-->
 </main>
@@ -71,33 +68,5 @@
 
 </html>
 
-<?php
-/**
- * Created by PhpStorm.
- * User: 0303077
- * Date: 25/04/2016
- * Time: 10:11
- */
 
-include('db_conn.php');
-$sql = "SELECT bugName, bugCategory, bugSummary
-        FROM bugs";
-$result = mysqli_query($conn,$sql);
-echo "
-<table>
-<tr>
-<th>Bug Name</th>
-<th>Bug Category</th>
-<th>Bug Summary</th>
-</tr> ";
 
-if(mysqli_num_rows($result)>0){
-    while($row = mysqli_fetch_assoc($result)){
-        echo "<tr>
-        <td>{$row['bugName']}</td>
-        <td>{$row['bugCategory']}</td>
-        <td>{$row['bugSummary']}</td>
-            </tr>";
-    }
-}
-echo "</table>";
