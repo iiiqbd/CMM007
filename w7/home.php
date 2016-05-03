8baf8be958a52d040de3021e2b68da20bf17348f
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-05-03 08:02:02 +0100

    ftrdfr

--- w7/home.php
+++ w7/home.php
@@ -14,10 +14,12 @@
 include("dbconnect.php"); //establishes connection with our database
 $sql = "SELECT * FROM users"; // displays all the users of the site
         $result = mysqli_query($db, $sql);
+        //echo "<p>These are the users currently logged in:</p>";
         while($row = mysqli_fetch_assoc($result)) {
             $username = $row['username'];
+            echo "<p>These are the users currently logged in:</p>";
             echo "<p>" . $username . "</p>";
-            //print_r($row);
+            //print_r($row); just prints the raw key->value pairs
         }
 
 /*
