b86ad03f237501dccf83fae073eb22c1c2a81c67
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-05-04 07:20:40 +0100

    ffgfn

--- w10/index.php
+++ w10/index.php
@@ -5,6 +5,14 @@
     <title>Bug Tracker</title>
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/unsemantic-grid-responsive-tablet.css">
+    <meta
+        name="viewport"
+        content="
+        width=device-width,
+        initial-scale=1,
+        minimum-scale=1,
+        maximum-scale=1"
+    />
 
 </head>
 <!-- start of body-->
@@ -18,10 +26,10 @@
     <!-- end of header-->
 
     <!-- start of main-->
-    <main>
+    <main class="grid-container">
 
         <!-- start of aside-->
-        <aside>
+        <aside class="grid-25">
             <!-- start of navigation-->
             <nav>
                 <ul>
@@ -37,7 +45,7 @@
         <!-- end of aside-->
 
         <!-- start of section-->
-        <section>
+        <section class="grid-75">
             <p>
 Lorem ipsum dolor sit amet, consectetur adipisjcing elit,sed do eiuassmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
